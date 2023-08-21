a6887b5fe4d2c3004ed3c4f7fdf158bfe31a0610
cordeirodessa <137825985+cordeirodessa@users.noreply.github.com>
2023-08-21 14:45:04 -0300

    adicionei contador de notas menores que seis

--- src/aula04/teste.java
+++ src/aula04/teste.java
@@ -6,25 +6,39 @@ public class teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		double nota1;
 		double nota2;
 		double nota3;
-		
+		int count = 0;
+		double nota;
+
 		System.out.println("Digite a primeira nota: ");
 		nota1 = sc.nextDouble();
-		
+
 		System.out.println("Digite a segunda nota: ");
 		nota2 = sc.nextDouble();
-		
+
 		System.out.println("Digite a terceira nota: ");
 		nota3 = sc.nextDouble();
-		
-		System.out.println("A média das notas é " + (nota1 + nota2 + nota3)/3);
-		
-		sc.close();
+
+		double media = (nota1 + nota2 + nota3) / 3;
+
+		System.out.println("A média das notas é " + media);
+
+		if (nota1 < 6) {
+			count++;
+		}
+		if (nota2 < 6) {
+			count++;
+		}
+		if (nota3 < 6) {
+			count++;
+		}
+
+		System.out.println("Quantidade de notas menores que 6: " + count);
+		return;
 	}
-			
 }
