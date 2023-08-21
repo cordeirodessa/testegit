e09f8940792ed0464ea4f7a1475f80dc3158672f
cordeirodessa <137825985+cordeirodessa@users.noreply.github.com>
2023-08-21 16:15:27 -0300

    Commit via https

--- src/aula04/teste.java
+++ src/aula04/teste.java
@@ -1,5 +1,7 @@
 package aula04;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Teste {
@@ -12,9 +14,11 @@ public class Teste {
 		double nota1;
 		double nota2;
 		double nota3;
+		double nota4;
+		double nota5;
 		int count = 0;
 		double nota;
-
+		
 		System.out.println("Digite a primeira nota: ");
 		nota1 = sc.nextDouble();
 
@@ -23,8 +27,16 @@ public class Teste {
 
 		System.out.println("Digite a terceira nota: ");
 		nota3 = sc.nextDouble();
-
-		double media = (nota1 + nota2 + nota3) / 3;
+		
+		System.out.println("Digite a quarta nota: ");
+		nota4 = sc.nextDouble();
+		
+		System.out.println("Digite a quinta nota: ");
+		nota5 = sc.nextDouble();
+					
+		double media = (nota1 + nota2 + nota3 + nota4 + nota5) / 3;
+		
+		nota = sc.nextDouble();
 
 		System.out.println("A média das notas é " + media);
 
@@ -37,11 +49,17 @@ public class Teste {
 		if (nota3 < 6) {
 			count++;
 		}
-
+		if (nota4 < 6) {
+			count++;
+		}
+		if (nota5 < 6) {
+			count++;
+		}
+		
 		System.out.println("Quantidade de notas menores que 6: " + count);
 				
-		double maiorNota = Math.max(nota1, Math.max(nota2, nota3));
-		double menorNota = Math.min(nota1, Math.min(nota2, nota3));
+		double maiorNota = Math.max(nota1, Math.max(nota2, Math.max(nota3, Math.max(nota4, nota5))));
+		double menorNota = Math.min(nota1, Math.min(nota2, Math.min(nota3, Math.min(nota4, nota5))));
 		
 		System.out.println("Maior nota: " + maiorNota);
 		System.out.println("Menor nota: " + menorNota);
